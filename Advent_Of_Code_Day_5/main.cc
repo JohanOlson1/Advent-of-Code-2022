6ca2729651f6b0017f9f75fc3c6f4c6ab261b6e4
Johan Olson <64142553+JohanOlson1@users.noreply.github.com>
2022-12-17 09:49:25 +0000

    Added day 5

--- Advent_Of_Code_Day_5/main.cc
+++ Advent_Of_Code_Day_5/main.cc
@@ -4,62 +4,90 @@
 #include <algorithm>
 #include <string>
 
-auto day4() {
-    std::ifstream myfile("../input.txt");
+void task1(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> &Stacks) {
+    for (int count = 0; count < N_to_mv; ++count) {
+        Stacks[stack_dest].push_back(Stacks[stack_src].back());
+        Stacks[stack_src].pop_back();
+    }
+} 
+
+void task2(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> &Stacks) { //RBTWJWMCF
+    std::string temp;
+    for (int count = 0; count < N_to_mv; ++count) {
+        temp.push_back(Stacks[stack_src].back());
+        Stacks[stack_src].pop_back();
+    }
+    for (int count = N_to_mv - 1; count >= 0; --count) {
+        Stacks[stack_dest].push_back(temp[count]);
+    }
+} 
+
+void day5() {
+    std::ifstream myfile("../input_day5.txt");
     std::string myline{};
-    int amount_of_contains{ 0 };
-    int amount_of_overlaps{ 0 };
-    int low_first{};
-    int high_first{};
-    int low_second{};
-    int high_second{};
+
+    std::vector<std::string> raw_input;
+    std::vector<std::string> Stacks{ 9 };
+    int stack{ 0 };
 
     if (myfile.is_open()) {
         std::cout << "File Opened\n";
-        while(myfile.good()) {
-            std::getline(myfile, myline, '-');
-            low_first = std::stoi(myline);
-            std::getline(myfile, myline, ',');
-            high_first = std::stoi(myline);
-            std::getline(myfile, myline, '-');
-            low_second = std::stoi(myline);
-            std::getline(myfile, myline);
-            high_second = std::stoi(myline);
-
-            if ((high_first - low_first) >= (high_second - low_second)) {
-                if (low_first <= low_second && high_first >= high_second) {
-                    ++amount_of_contains;
-                }
-            } else {
-                if (low_second <= low_first && high_second >= high_first) {
-                    ++amount_of_contains;
-                }
-            }
-            
-            if ((high_first - low_first) >= (high_second - low_second)) {
-                if ((low_first <= low_second && low_second <= high_first) || (low_second <= low_first && low_first <= high_second)) {
-                    ++amount_of_overlaps;
-                }
-            } else {
-                if ((low_second <= low_first && low_first <= high_second) || (low_first <= low_second && low_second <= high_first)) {
-                    ++amount_of_overlaps;
-                }
-            }
+        while(std::getline(myfile, myline) && !myline.empty()) { // Read the data into an vector of strings
+            raw_input.push_back(myline);
         }
+
+        int height = raw_input.size();
+        int length = raw_input[0].size();
+        int stack{ 0 };
+        for (int index = 1; index <= length; index += 4) { // Read over the data s.t. we get a vector or each stack with the content in correct order
+            for (int heigh{height - 1}; heigh >= 0; --heigh) {
+                if( raw_input[heigh][index] != ' ') {
+                    Stacks[stack].push_back(raw_input[heigh][index]);
+                }
+            }
+            ++stack;
+        }
+
+        std::vector<std::string> Stacks2 = Stacks; // Duplicate the Stacks for the two different tasks
+
+        int N_to_mv;
+        int stack_src;
+        int stack_dest;
+        while(myfile.good()) { // Perform operation based on inputs until end file
+            std::getline(myfile, myline, ' ');
+            std::getline(myfile, myline, ' ');
+            N_to_mv = std::stoi(myline);
+
+            std::getline(myfile, myline, ' ');
+            std::getline(myfile, myline, ' ');
+            stack_src = std::stoi(myline) - 1;
+
+            std::getline(myfile, myline, ' ');
+            std::getline(myfile, myline);
+            stack_dest = std::stoi(myline) - 1;
+
+            task1(N_to_mv, stack_src, stack_dest, Stacks); 
+            task2(N_to_mv, stack_src, stack_dest, Stacks2); 
+        }
+
+        std::cout << "In task 1 the top creates are: ";
+        for (int count = 0; count < Stacks.size(); ++ count) { // Display result task1
+            std::cout << Stacks[count].back();
+        }
+        std::cout << "\n";
+
+        std::cout << "In task 2 the top creates are: ";
+        for (int count = 0; count < Stacks.size(); ++ count) { // Display result task 2
+            std::cout << Stacks2[count].back();
+        }
+        std::cout << "\n";
+    } else {
+        std::cout << "Failed to open file\n"; 
     }
-    else {
-        std::cout << "Failed to load file\n";
-    }
-  struct retVals {        // Declare a local structure 
-    int i1, i2;
-  };
-  return retVals {amount_of_contains, amount_of_overlaps}; // Return the local structure
 }
 
 int main() {
-    auto [amount_of_contains, amount_of_overlaps] = day4(); // structured binding declaration
+    day5();
 
-    std::cout << "Amount of containments: " << amount_of_contains << "\n";
-    std::cout << "Amount of containments: " << amount_of_overlaps << "\n";
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# This is a project to solve some Advent of Code problems
+Each day is included as a directory and the main.cc containst the implementation <br>
