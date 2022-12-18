56842ec19822cf522c9e85f18144ac59e95d5b8b
Johan Olson <64142553+JohanOlson1@users.noreply.github.com>
2022-12-18 10:21:46 +0000

    Refactoring day 4-5

--- Advent_Of_Code_Day_4/main.cc
+++ Advent_Of_Code_Day_4/main.cc
@@ -4,58 +4,72 @@
 #include <algorithm>
 #include <string>
 
-auto day4() {
-    std::ifstream myfile("../input.txt");
-    std::string myline{};
+// Function forward declaration
+int checkContain(const int low_large, const int high_large, const int low_small, const int high_small);
+int checkOverlap(const int low_large, const int high_large, const int low_small, const int high_small);
+std::pair<int, int> day4(const std::string input_file_path);
+
+int main() {
+    const std::string input_file_path{"../input_day4.txt"};
+    std::pair<int, int> day4_answers = day4(input_file_path); 
+
+    std::cout << "Task 1: " << day4_answers.first  << " contains\n";
+    std::cout << "Task 2: " << day4_answers.second << " overlaps\n";
+    return 0;
+}
+
+// Function definition
+int checkContain(const int low_large, const int high_large, const int low_small, const int high_small) {
+    if (low_large <= low_small && high_small <= high_large) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int checkOverlap(const int low_large, const int high_large, const int low_small, const int high_small) {
+    if ((low_large <= low_small && low_small <= high_large) || (low_small <= low_large && low_large <= high_small)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+std::pair<int, int> day4(const std::string input_file_path) {
+    std::ifstream my_file(input_file_path);
+    std::string my_line{};
     int amount_of_contains{ 0 };
     int amount_of_overlaps{ 0 };
     int low_first{};
     int high_first{};
     int low_second{};
     int high_second{};
+    int largest_interval{};
 
-    if (myfile.is_open()) {
-        std::cout << "File Opened\n";
-        while(myfile.good()) {
-            std::getline(myfile, myline, '-');
-            low_first = std::stoi(myline);
-            std::getline(myfile, myline, ',');
-            high_first = std::stoi(myline);
-            std::getline(myfile, myline, '-');
-            low_second = std::stoi(myline);
-            std::getline(myfile, myline);
-            high_second = std::stoi(myline);
+    if (my_file.is_open()) {
+        std::cout << "Input file opened\n";
+        while(my_file.good()) {
+            std::getline(my_file, my_line, '-');
+            low_first = std::stoi(my_line);
+            std::getline(my_file, my_line, ',');
+            high_first = std::stoi(my_line);
+            std::getline(my_file, my_line, '-');
+            low_second = std::stoi(my_line);
+            std::getline(my_file, my_line);
+            high_second = std::stoi(my_line);
 
-            if ((high_first - low_first) >= (high_second - low_second)) {
-                if (low_first <= low_second && high_first >= high_second) {
-                    ++amount_of_contains;
-                }
-                if ((low_first <= low_second && low_second <= high_first) || (low_second <= low_first && low_first <= high_second)) {
-                    ++amount_of_overlaps;
-                }
-            } else {
-                if (low_second <= low_first && high_second >= high_first) {
-                    ++amount_of_contains;
-                }
-                if ((low_second <= low_first && low_first <= high_second) || (low_first <= low_second && low_second <= high_first)) {
-                    ++amount_of_overlaps;
-                }
+            largest_interval = ((high_first - low_first) > (high_second - low_second)); // 1 the first is largest, 0 the second is largest
+            switch (largest_interval) {
+                case 1: amount_of_contains += checkContain(low_first, high_first, low_second, high_second); 
+                        amount_of_overlaps += checkOverlap(low_first, high_first, low_second, high_second); break;
+                case 0: amount_of_contains += checkContain(low_second, high_second, low_first, high_first);
+                        amount_of_overlaps += checkOverlap(low_second, high_second, low_first, high_first); break;
             }
         }
     }
     else {
-        std::cout << "Failed to load file\n";
+        std::cout << "Failed to load input file\n";
     }
-  struct retVals {        // Declare a local structure 
-    int i1, i2;
-  };
-  return retVals {amount_of_contains, amount_of_overlaps}; // Return the local structure
-}
 
-int main() {
-    auto [amount_of_contains, amount_of_overlaps] = day4(); // structured binding declaration
-
-    std::cout << "Amount of containments: " << amount_of_contains << "\n";
-    std::cout << "Amount of containments: " << amount_of_overlaps << "\n";
-    return 0;
+  return std::make_pair(amount_of_contains, amount_of_overlaps);
 }
--- Advent_Of_Code_Day_5/main.cc
+++ Advent_Of_Code_Day_5/main.cc
@@ -4,6 +4,24 @@
 #include <algorithm>
 #include <string>
 
+// Function forward declaration
+void task1(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> &Stacks);
+void task2(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> &Stacks);
+std::vector<std::string> readCrateDataToStacks(const std::vector<std::string> &raw_input);
+void parseFileAndMoveCrates(std::ifstream &my_file, std::vector<std::string> &Stacks_task1, std::vector<std::string> &Stacks_task2);
+std::pair<std::string, std::string> day5(const std::string input_file_path);
+
+int main() {
+    const std::string input_file_path{"../input_day5.txt"};
+    std::pair<std::string, std::string> day5_answers = day5(input_file_path);
+
+    std::cout << "Task 1: " << day5_answers.first << " are top crates\n";
+    std::cout << "Task 2: " << day5_answers.second << " are top crates\n";
+
+    return 0;
+}
+
+// Function defintion
 void task1(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> &Stacks) {
     for (int count = 0; count < N_to_mv; ++count) {
         Stacks[stack_dest].push_back(Stacks[stack_src].back());
@@ -11,7 +29,7 @@ void task1(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> 
     }
 } 
 
-void task2(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> &Stacks) { //RBTWJWMCF
+void task2(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> &Stacks) {
     std::string temp;
     for (int count = 0; count < N_to_mv; ++count) {
         temp.push_back(Stacks[stack_src].back());
@@ -20,74 +38,75 @@ void task2(int N_to_mv, int stack_src, int stack_dest, std::vector<std::string> 
     for (int count = N_to_mv - 1; count >= 0; --count) {
         Stacks[stack_dest].push_back(temp[count]);
     }
-} 
+}
 
-void day5() {
-    std::ifstream myfile("../input_day5.txt");
-    std::string myline{};
+std::vector<std::string> readCrateDataToStacks(const std::vector<std::string> &raw_input) {
+    int height = raw_input.size();
+    int length = raw_input[0].size();
+    int stack_amount{ (length + 1)/4 };
+    std::vector<std::string> Stacks{ static_cast<long unsigned int>(stack_amount) }; // Kinda ugly, atleast brace intialization warns us of this
 
-    std::vector<std::string> raw_input;
-    std::vector<std::string> Stacks{ 9 };
     int stack{ 0 };
-
-    if (myfile.is_open()) {
-        std::cout << "File Opened\n";
-        while(std::getline(myfile, myline) && !myline.empty()) { // Read the data into an vector of strings
-            raw_input.push_back(myline);
-        }
-
-        int height = raw_input.size();
-        int length = raw_input[0].size();
-        int stack{ 0 };
-        for (int index = 1; index <= length; index += 4) { // Read over the data s.t. we get a vector or each stack with the content in correct order
-            for (int heigh{height - 1}; heigh >= 0; --heigh) {
-                if( raw_input[heigh][index] != ' ') {
-                    Stacks[stack].push_back(raw_input[heigh][index]);
-                }
+    for (int index = 1; index <= length; index += 4) { // Read over the data s.t. we get a vector of each stack with the content in correct order
+        for (int heigh{height - 1}; heigh >= 0; --heigh) {
+            if( raw_input[heigh][index] != ' ') {
+                Stacks[stack].push_back(raw_input[heigh][index]);
             }
-            ++stack;
         }
+        ++stack;
+    }
 
-        std::vector<std::string> Stacks2 = Stacks; // Duplicate the Stacks for the two different tasks
+    return Stacks;
+}
 
-        int N_to_mv;
-        int stack_src;
-        int stack_dest;
-        while(myfile.good()) { // Perform operation based on inputs until end file
-            std::getline(myfile, myline, ' ');
-            std::getline(myfile, myline, ' ');
-            N_to_mv = std::stoi(myline);
+void parseFileAndMoveCrates(std::ifstream &my_file, std::vector<std::string> &Stacks_task1, std::vector<std::string> &Stacks_task2) {
+    int N_to_mv;
+    int stack_src;
+    int stack_dest;
+    std::string my_line{};
+    while(my_file.good()) { // Perform operation based on inputs until end file
+        std::getline(my_file, my_line, ' ');
+        std::getline(my_file, my_line, ' ');
+        N_to_mv = std::stoi(my_line);
 
-            std::getline(myfile, myline, ' ');
-            std::getline(myfile, myline, ' ');
-            stack_src = std::stoi(myline) - 1;
+        std::getline(my_file, my_line, ' ');
+        std::getline(my_file, my_line, ' ');
+        stack_src = std::stoi(my_line) - 1;
 
-            std::getline(myfile, myline, ' ');
-            std::getline(myfile, myline);
-            stack_dest = std::stoi(myline) - 1;
+        std::getline(my_file, my_line, ' ');
+        std::getline(my_file, my_line);
+        stack_dest = std::stoi(my_line) - 1;
 
-            task1(N_to_mv, stack_src, stack_dest, Stacks); 
-            task2(N_to_mv, stack_src, stack_dest, Stacks2); 
-        }
-
-        std::cout << "In task 1 the top creates are: ";
-        for (int count = 0; count < Stacks.size(); ++ count) { // Display result task1
-            std::cout << Stacks[count].back();
-        }
-        std::cout << "\n";
-
-        std::cout << "In task 2 the top creates are: ";
-        for (int count = 0; count < Stacks.size(); ++ count) { // Display result task 2
-            std::cout << Stacks2[count].back();
-        }
-        std::cout << "\n";
-    } else {
-        std::cout << "Failed to open file\n"; 
+        task1(N_to_mv, stack_src, stack_dest, Stacks_task1); 
+        task2(N_to_mv, stack_src, stack_dest, Stacks_task2); 
     }
 }
 
-int main() {
-    day5();
+std::pair<std::string, std::string> day5(const std::string input_file_path) {
+    std::pair<std::string, std::string> top_crates;
+    std::ifstream my_file(input_file_path);
 
-    return 0;
+    std::string my_line{};
+    std::vector<std::string> raw_input;
+
+    if (my_file.is_open()) {
+        std::cout << "File Opened\n";
+        while(std::getline(my_file, my_line) && !my_line.empty()) { // Read the crate input data into a vector of strings
+            raw_input.push_back(my_line);
+        }
+
+        std::vector<std::string> Stacks_task1 = readCrateDataToStacks(raw_input); 
+        std::vector<std::string> Stacks_task2 = readCrateDataToStacks(raw_input); // Not a comp. expensive operation so can duplicate
+
+        parseFileAndMoveCrates(my_file, Stacks_task1, Stacks_task2);
+
+        for (int count = 0; count < Stacks_task1.size(); ++count) {
+            top_crates.first.push_back(Stacks_task1[count].back());
+            top_crates.second.push_back(Stacks_task2[count].back());
+        }
+    } else {
+        std::cout << "Failed to open file\n"; 
+    }
+
+    return top_crates;
 }
